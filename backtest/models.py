16f11be02f9cfe3e2785eea80c7e02653b82a52c
ahmetyasinc <ahmetyasinc26@gmail.com>
2024-10-24 14:04:56 +0300

    User-ındicator Macth

--- backtest/models.py
+++ backtest/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Indicator(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     code = models.TextField()
     on_graph = models.BooleanField(default=True)
--- backtest/views.py
+++ backtest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import MyForm, IndicatorForm
 from .models import Indicator
+from django.http import HttpResponseForbidden
 import yfinance as yf
 
 coinler = ["bitcoin","ethereum","pepe"]
@@ -49,7 +50,9 @@ def indicator_create(request):
     if request.method == 'POST':
         form = IndicatorForm(request.POST)
         if form.is_valid():
-            form.save()  # Formu kaydet ve veritabanına ekle
+            indicator = form.save(commit=False)
+            indicator.user = request.user  # İndikatörü oluşturan kullanıcıyı ekle
+            indicator.save()
             return redirect('indicator_list')  # Form gönderildiğinde yönlendirme yapılacak bir sayfa
     else:
         form = IndicatorForm()
@@ -57,5 +60,10 @@ def indicator_create(request):
     return render(request, 'indicator_form.html', {'form': form})
 
 def indicator_list(request):
-    indicators = Indicator.objects.all()  # Tüm Indicator kayıtlarını getir
+    indicators = Indicator.objects.filter(user=request.user)  # Tüm Indicator kayıtlarını getir
     return render(request, 'indicator_list.html', {'indicators': indicators})
+
+def edit_indicator(request, indicator_id):
+    indicator = Indicator.objects.get(id=indicator_id)
+    if indicator.user != request.user:
+        return HttpResponseForbidden()
